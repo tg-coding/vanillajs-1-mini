b3a7b5372629c01c9f3b5faee06d23c4fe0fe9be
Tia Gollaher <tiaag2013@gmail.com>
2020-01-01 15:46:31 -0700

    finished vanilla js 1 mini

---
+++ index.js
@@ -0,0 +1,30 @@
+// console.log('hello world')
+
+let count = 0;
+let element = document.getElementById('counter');
+
+
+function increase(){
+    count++;
+    element.innerText = count;
+} 
+
+function decrease(){
+    count--;
+    element.innerText = count;
+}
+
+function reset(){
+    count = 0;
+    element.innerHTML = "<mark>" + count + "</mark>";
+}
+
+function selectTheme(theme){
+    document.getElementsByTagName('body')[0].className = theme;
+    document.getElementsByTagName('main'[0]).className = theme;
+    const buttons = document.getElementsByTagName('button');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].className = theme;
+    }
+}
